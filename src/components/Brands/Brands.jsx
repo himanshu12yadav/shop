fd436652b5f369bc5df77bdbacac679c7044434a
himanshuyadav.12jan <himanshuyadav.12jan@gmail.com>
2025-02-01 06:08:13 +0000

    11:37:AM-saturday-01/02/2025

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -4,52 +4,53 @@ import BrandList from "./BrandList/BrandList";
 export default function Brands(){
 
     const brands = [
-            {
+        {
             id: 1,
             name: 'Nike',
-            image: 'https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Logo_NIKE.svg'
         },
         {
             id: 2,
             name: 'Adidas',
-            image: 'https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            image: 'https://upload.wikimedia.org/wikipedia/commons/2/20/Adidas_Logo.svg'
         },
         {
             id: 3,
             name: 'Puma',
-            image: 'https://images.pexels.com/photos/15'
+            image: 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Puma-Logo.svg'
         },
         {
             id: 4,
             name: 'Reebok',
-            image: 'https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            image: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Reebok_2019_logo.svg'
         },
         {
             id: 5,
             name: 'New Balance',
-            image: 'https://images.pexels.com/photos/159850'
+            image: 'https://upload.wikimedia.org/wikipedia/commons/4/4e/New_Balance_logo.svg'
         },
         {
             id: 6,
             name: 'Converse',
-            image: 'https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            image: 'https://upload.wikimedia.org/wikipedia/commons/3/3f/Converse_logo.svg'
         },
         {
             id: 7,
             name: 'Vans',
-            image: 'https://images.pexels.com/photos/1598505/pexels'
+            image: 'https://upload.wikimedia.org/wikipedia/commons/e/e5/Vans-logo.svg'
         },
         {
             id: 8,
             name: 'Fila',
-            image: 'https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            image: 'https://upload.wikimedia.org/wikipedia/commons/0/03/Fila_logo.svg'
         },
         {
             id: 9,
             name: 'Skechers',
-            image: 'https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            image: 'https://upload.wikimedia.org/wikipedia/commons/3/3b/Skechers_logo.svg'
         }
-        ]
+    ];
+    
 
     return (
         <div className={styles.brands}>
