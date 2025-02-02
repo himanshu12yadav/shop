263761b273810c1a20b18e1c5f4e0b3bcc086af1
himanshu12yadav <himanshuyadav.12jan@gmail.com>
2025-02-02 19:22:29 +0000

    00:44-sunday-03/02/25

--- src/components/Collection/ProductCard/ProductCard.jsx
+++ src/components/Collection/ProductCard/ProductCard.jsx
@@ -3,7 +3,7 @@ import styles from './ProductCard.module.scss';
 export default function ProductCard(){
     return (
         <div>
-            <h3>Product card</h3>
+            <h3>Product Collection</h3>
         </div>
     )
 }
